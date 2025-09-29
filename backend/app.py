3ae0f251770236044e861cffdf94d5ad17397258
chaimaeBky <chaimaebky14@gmail.com>
2025-09-29 16:27:34 +0100

    actions workflows

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,20 @@
+import os
 from flask import Flask, jsonify
 import requests
 from flask_cors import CORS  
 
 app = Flask(__name__) 
-CORS(app, origins=["http://react-frontend-unique123.eastus.azurecontainer.io"])
+CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "http://react-frontend-unique123.eastus.azurecontainer.io",
+            "http://localhost:5173",
+            "http://localhost:3000"
+        ],
+        "methods": ["GET", "POST", "OPTIONS"],
+        "allow_headers": ["Content-Type"]
+    }
+})
 
 @app.route('/rates' , methods=['GET'])
 def getRates() :
